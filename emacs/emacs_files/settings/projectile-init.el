e476c8624c014582e47e963b4719fede8eff72a0
daniel5908 <danth343@student.liu.se>
2019-10-20 11:34:14 +0200

    Emacs conf now works, added outcommented lines to ignore signature failure if needed in the future

--- emacs/emacs_files/settings/projectile-init.el
+++ emacs/emacs_files/settings/projectile-init.el
@@ -1,5 +1,9 @@
 
-(projectile-global-mode)
-(counsel-projectile-mode)
+;;(projectile-global-mode)
+;;(counsel-projectile-mode)
+
+(projectile-mode +1)
+(define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
 
 (provide 'projectile-init)
