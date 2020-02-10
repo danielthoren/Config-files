441afe4c49b458f2e7f87bd7fe475ee9e08661ee
daniel5908 <danth343@student.liu.se>
2020-02-10 11:19:19 +0100

    Added password query to server_connect.sh script

--- fish/fish_files/functions/server_connect.sh
+++ fish/fish_files/functions/server_connect.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 global_ip="155.4.155.164"
-user=daniel5908
 sshPort=2022
-#todo: add password input
-#password=
+user=daniel5908
 
-rm ~/.bash_history
+echo "Enter password:"
+read -s password
 
 konsole -e sshpass -p $password ssh -p $sshPort -L 8081:192.168.1.100:8081 $user@$global_ip &
 konsole -e sshpass -p $password ssh -p $sshPort -L 80:192.168.1.100:80 $user@$global_ip &
