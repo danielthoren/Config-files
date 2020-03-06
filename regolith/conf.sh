78ed7c753ead5491ad19bc1fc33bb19a3d898f83
danielthoren <danne_thoren456@hotmail.com>
2020-03-06 16:21:57 +0100

    Added regolith files to repo

---
+++ regolith/conf.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+dir=~/.config/regolith
+
+source ../functions.sh
+
+echo "Configuring regolith in folder $dir"
+
+if [ ! -d $dir ]; then
+    echo "Folder does not exist, creating folder"
+    mkdir -p $dir
+else
+    echo "Folder exists, purging data"
+    rm -r "${dir}/i3"
+    rm -r "${dir}/i3xrocks"
+fi
+
+ln -s "${PWD}/regolith_files/i3" "${dir}"
+ln -s "${PWD}/regolith_files/i3xrocks" "${dir}"
+ln -s "${PWD}/regolith_files/conf.sh" "${dir}"
+
+echo "Done configuring regolith"
