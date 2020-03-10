047e3d09441c06b7dcba7c1a532e5581512dcf7f
danielthoren <danne_thoren456@hotmail.com>
2020-03-10 22:38:44 +0100

    Added opacity to emacs. Also added hide bold markers etc in org mode. Fixed C-, key-binding for org mode by using 'use-package' key-binding

--- emacs/emacs_files/settings/base-settings.el
+++ emacs/emacs_files/settings/base-settings.el
@@ -18,4 +18,26 @@
 ;;(setq backup-directory-alist '(("" . (expand-file-name "backup" user-emacs-directory))))
 
 
+;; Set opacity
+
+;;(set-frame-parameter (selected-frame) 'alpha '(<active> . <inactive>))
+;;(set-frame-parameter (selected-frame) 'alpha <both>)
+(set-frame-parameter (selected-frame) 'alpha '(95 . 80))
+(add-to-list 'default-frame-alist '(alpha . (95 . 80)))
+
+;; Toggle opacity
+(defun toggle-transparency ()
+  (interactive)
+  (let ((alpha (frame-parameter nil 'alpha)))
+    (set-frame-parameter
+     nil 'alpha
+     (if (eql (cond ((numberp alpha) alpha)
+		    ((numberp (cdr alpha)) (cdr alpha))
+		    ;; Also handle undocumented (<active> <inactive>) form.
+		    ((numberp (cadr alpha)) (cadr alpha)))
+	      100)
+	 '(85 . 50) '(100 . 100)))))
+(global-set-key (kbd "C-c t") 'toggle-transparency)
+
+
 (provide 'base-settings)
--- emacs/emacs_files/settings/markdown-init.el
+++ emacs/emacs_files/settings/markdown-init.el
@@ -1,4 +1,7 @@
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 (setq markdown-header-scaling t)
 
+(setq org-hide-emphasis-markers t)
+(setq org-fontify-done-headline t)
+
 (provide 'markdown-init)
