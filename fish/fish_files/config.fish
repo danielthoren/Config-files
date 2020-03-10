00c8e3a4fb9bbf743f5793054ecc602018b63a6f
Daniel Thoren <danth343@student.liu.se>
2020-03-10 18:41:54 +0100

    Fixed backlight bindings for i3. Had to edit /etc/X11/xorg.conf fo make xbacklight work properly

--- fish/fish_files/config.fish
+++ fish/fish_files/config.fish
@@ -4,6 +4,9 @@ export EDITOR='emacs'
 alias arduino="/home/daniel5908/programs/arduino-1.8.10/arduino"
 alias javac_pack="/home/daniel5908/git/Config-files/fish/fish_files/functions/javac_package.sh"
 
+#fix grep match color highlight
+export GREP_OPTIONS='--color=auto'
+
 alias
 alias
 clear
