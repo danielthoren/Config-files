ef3a1926bacbc62f2d8288bd45554ffaa648ace7
danielthoren <danne_thoren456@hotmail.com>
2020-04-07 11:51:32 +0200

    Added alias to fish files for wacom device and added alias for clear in kitty. Now clears scroll back buffer

--- fish/fish_files/config.fish
+++ fish/fish_files/config.fish
@@ -5,6 +5,8 @@ export EDITOR='emacs'
 alias arduino="/home/daniel5908/programs/arduino-1.8.10/arduino"
 alias javac_pack="/home/daniel5908/git/Config-files/fish/fish_files/functions/javac_package.sh"
 
+alias wacommap="/home/daniel5908/git/Config-files/fish/fish_files/functions/map_wacom.sh"
+
 alias
 alias
 clear
---
+++ fish/fish_files/functions/map_wacom.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+xinput map-to-output 12 DVI-D-0
+xinput map-to-output 10 DVI-D-0
+xinput map-to-output 11 DVI-D-0
+xinput map-to-output 15 DVI-D-0
+xinput map-to-output 16 DVI-D-0
+  
