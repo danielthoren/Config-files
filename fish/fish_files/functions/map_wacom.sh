a9fe765b621317c612324c6994a798c2efb6792b
danielthoren <danne_thoren456@hotmail.com>
2020-04-20 09:32:31 +0200

    Fixed wacom remap script, now uses regex to extract ids from xinput

--- fish/fish_files/functions/map_wacom.sh
+++ fish/fish_files/functions/map_wacom.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-xinput map-to-output 12 DVI-D-0
-xinput map-to-output 10 DVI-D-0
-xinput map-to-output 11 DVI-D-0
-xinput map-to-output 15 DVI-D-0
-xinput map-to-output 16 DVI-D-0
+for num in $(xinput list | grep Wacom | awk '{print $9}' | sed $num 's/id=//');
+do
+    echo $num
+    xinput map-to-output $num DVI-D-0
+done
+
+
+# xinput map-to-output 12 DVI-D-0
+# xinput map-to-output 10 DVI-D-0
+# xinput map-to-output 11 DVI-D-0
+# xinput map-to-output 15 DVI-D-0
+# xinput map-to-output 16 DVI-D-0
   
