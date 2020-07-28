566cb3cc80b3922dcfe8dd48816c6902b414b40c
danielthoren <danne_thoren456@hotmail.com>
2020-07-28 10:56:42 +0200

    Added script for remote server mounting accepting flags for local or global connections

--- fish/fish_files/functions/server.fish
+++ fish/fish_files/functions/server.fish
@@ -1,11 +1,7 @@
-alias sfconnl="sudo sshfs -o allow_other -p 2022 daniel5908@192.168.1.100:/srv/dev-disk-by-label-storage /mnt/server ; \
-               sudo sshfs -o allow_other -p 2022 daniel5908@192.168.1.100:/srv/dev-disk-by-label-download /mnt/download"
 
-alias sfconn="sudo sshfs -o allow_other -p 2022 daniel5908@155.4.155.164:/srv/dev-disk-by-label-storage /mnt/server ;
-              sudo sshfs -o allow_other -p 2022 daniel5908@155.4.155.164:/srv/dev-disk-by-label-download /mnt/download"
+alias sfconn="~/.config/fish/functions/server_mount.sh"
 
 alias sfdiss="sudo umount -f /mnt/server ; \
               sudo umount -f /mnt/download"
-              
 
 alias swconn="~/.config/fish/functions/server_connect.sh"
