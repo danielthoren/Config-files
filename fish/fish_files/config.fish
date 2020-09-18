6162bad66dc6190af905ca1446b8ff1abd541cfc
daniel5908 <danne_thoren456@hotmail.com>
2020-09-18 12:34:36 +0200

    Changed labass mode in emacs to use english and added lab correction script for lab1 in tdde22

--- fish/fish_files/config.fish
+++ fish/fish_files/config.fish
@@ -6,7 +6,7 @@ export FIFUNC='/home/daniel5908/git/Config-files'
 alias arduino="/home/daniel5908/programs/arduino-1.8.10/arduino"
 alias javac_pack="/home/daniel5908/git/Config-files/fish/fish_files/functions/javac_package.sh"
 
-alias wacommap="/home/daniel5908/git/Config-files/fish/fish_files/functions/map_wacom.sh"
+alias lab1_tdde22="/home/daniel5908/git/Config-files/fish/fish_files/functions/lab1_tdde22.sh"
 
 alias
 alias
