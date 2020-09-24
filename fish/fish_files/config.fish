72176f13d5b03ba729cc7c9e95571b628b3096bd
danielthoren <danne_thoren456@hotmail.com>
2020-09-24 11:02:25 +0200

    Added correction script for lab2 tdde22

--- fish/fish_files/config.fish
+++ fish/fish_files/config.fish
@@ -7,6 +7,7 @@ alias arduino="/home/daniel5908/programs/arduino-1.8.10/arduino"
 alias javac_pack="/home/daniel5908/git/Config-files/fish/fish_files/functions/javac_package.sh"
 
 alias lab1_tdde22="/home/daniel5908/git/Config-files/fish/fish_files/functions/lab1_tdde22.sh"
+alias lab2_tdde22="/home/daniel5908/git/Config-files/fish/fish_files/functions/lab2_tdde22.sh"
 
 alias
 alias
