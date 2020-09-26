3d9d44ed6ad9aa029f5751002e6848aad07f384e
daniel5908 <danne_thoren456@hotmail.com>
2020-09-26 15:51:11 +0200

    Fixed problems with mount and unmount scripts for server

--- fish/fish_files/functions/server.fish
+++ fish/fish_files/functions/server.fish
@@ -1,7 +1,8 @@
 
 alias sfconn="~/.config/fish/functions/server_mount.sh"
 
-alias sfdiss="sudo umount -f /mnt/server ; \
-              sudo umount -f /mnt/download"
+alias sfdiss="sudo umount -f ~/server/download ; \
+              sudo umount -f ~/server/storage ; \
+	      sudo umount -f ~/labbass"
 
 alias swconn="~/.config/fish/functions/server_connect.sh"
