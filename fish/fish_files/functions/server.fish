f7084899523ea1306c5f4fffd7bf3072fb3bdeea
danielthoren <danne_thoren456@hotmail.com>
2020-09-27 13:51:42 +0200

    Moved sfdiss to own bash script and added call to it from sfconn script

--- fish/fish_files/functions/server.fish
+++ fish/fish_files/functions/server.fish
@@ -1,8 +1,6 @@
 
 alias sfconn="~/.config/fish/functions/server_mount.sh"
 
-alias sfdiss="sudo umount -f ~/server/download ; \
-              sudo umount -f ~/server/storage ; \
-	      sudo umount -f ~/labbass"
+alias sfdiss="~/.config/fish/functions/server_diss.sh"
 
 alias swconn="~/.config/fish/functions/server_connect.sh"
