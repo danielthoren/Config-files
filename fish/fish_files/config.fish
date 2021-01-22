d87a81d76f60a419f0e2765e74207709e1df9e9b
Daniel Thorén <danth343@student.liu.se>
2021-01-22 09:21:43 +0100

    Added path export and fixed bugs in scripts

--- fish/fish_files/config.fish
+++ fish/fish_files/config.fish
@@ -1,17 +1,11 @@
 
-set PATH /home/daniel5908/git/pintos/src/utils $PATH
+set PATH ~/.local/bin $PATH
 export EDITOR='emacs'
 export FIFUNC='/home/daniel5908/git/Config-files'
 
 alias arduino="/home/daniel5908/programs/arduino-1.8.10/arduino"
-alias javac_pack="/home/daniel5908/git/Config-files/fish/fish_files/functions/javac_package.sh"
 
-alias lab1_tdde22="/home/daniel5908/git/Config-files/fish/fish_files/functions/lab1_tdde22.sh"
-alias lab2_tdde22="/home/daniel5908/git/Config-files/fish/fish_files/functions/lab2_tdde22.sh"
-alias lab3_tdde22="/home/daniel5908/git/Config-files/fish/fish_files/functions/lab3_tdde22.sh"
-alias lab4_tdde22="/home/daniel5908/git/Config-files/fish/fish_files/functions/lab4_tdde22.sh"
-
-sfconn
+alias logout="gnome-screensaver-command -l"
 
 alias
 alias
--- fish/fish_files/functions/server_connect.sh
+++ fish/fish_files/functions/server_connect.sh
@@ -7,7 +7,6 @@ konsole -e sshpass -p $passw ssh -p $ssh_port -L 80:192.168.1.100:80 $user@$glob
 konsole -e sshpass -p $passw ssh -p $ssh_port -L 8112:192.168.1.100:8112 $user@$global_ip &
 konsole -e sshpass -p $passw ssh -p $ssh_port -L 5050:192.168.1.100:5050 $user@$global_ip &
 konsole -e sshpass -p $passw ssh -p $ssh_port -L 5000:192.168.1.214:5000 $user@$global_ip &
-konsole -e sshpass -p $passw ssh -p $ssh_port -L 8006:192.168.1.7:8006 $user@$global_ip &
+konsole -e sshpass -p $passw ssh -p $ssh_port -L 8006:192.168.1.186:8006 $user@$global_ip &
 konsole -e sshpass -p $passw ssh -p $ssh_port -L 9117:192.168.1.100:9117 $user@$global_ip &
 konsole -e sshpass -p $passw ssh -p $ssh_port -L 8080:192.168.1.100:8080 $user@$global_ip &
-
