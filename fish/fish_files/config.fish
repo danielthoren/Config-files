339a8c280ae35c132759edaeb026c1eae5dc235e
daniel <danne_thoren456@hotmail.com>
2021-01-28 10:44:00 +0100

    Added list usb device script and removed unecessary functions

--- fish/fish_files/config.fish
+++ fish/fish_files/config.fish
@@ -1,11 +1,21 @@
 
 set PATH ~/.local/bin $PATH
+
 export EDITOR='emacs'
 export FIFUNC='/home/daniel5908/git/Config-files'
+
+alias c++17="g++ -Wall -Wextra -Weffc++ -Wold-style-cast -Woverloaded-virtual -std=c++17 -pedantic"
 
 alias arduino="/home/daniel5908/programs/arduino-1.8.10/arduino"
 
 alias logout="gnome-screensaver-command -l"
+
+alias usbl="~/.config/fish/functions/list_usb.sh"
+
+
+alias sfconn="~/.config/fish/functions/server_mount.sh"
+alias sfdiss="~/.config/fish/functions/server_diss.sh"
+alias swconn="~/.config/fish/functions/server_connect.sh"
 
 alias
 alias
--- fish/fish_files/functions/server.fish
+++
@@ -1,6 +0,0 @@
-
-alias sfconn="~/.config/fish/functions/server_mount.sh"
-
-alias sfdiss="~/.config/fish/functions/server_diss.sh"
-
-alias swconn="~/.config/fish/functions/server_connect.sh"
