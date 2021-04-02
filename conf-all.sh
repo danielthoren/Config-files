0d154258e23edc3ecc19e1b972079d3ad9fa425b
daniel5908 <danne_thoren456@hotmail.com>
2021-04-02 23:34:20 +0200

    Added installation of essential programs to conf all script

--- conf-all.sh
+++ conf-all.sh
@@ -4,6 +4,17 @@
 # -no-sudo  (for installation wighout sudo)
 # -laptop   (for installation with laptop specific settings)
 
+source functions.sh
+
+update
+$APT_UPGRADE
+
+install htop
+install git
+install gparted
+install net-tools
+install sshpass
+
 cd emacs
 ./conf.sh -all-conf "$@"
 cd ../fish
@@ -11,6 +22,4 @@ cd ../fish
 cd ../kitty
 .conf.sh -all-conf "$@"
 cd ../ranger
-./conf.sh -all-conf "$@"
-cd ../regolith
 ./conf.sh -all-conf "$@"
