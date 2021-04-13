d75e1955d7343ed69a40e75d3afb392ce032bca1
danielthoren <danne_thoren456@hotmail.com>
2021-04-13 09:21:34 +0200

    emacs: Removed flycheck-global-mode and added flycheck to python.init

--- emacs/emacs_files/settings/python-init.el
+++ emacs/emacs_files/settings/python-init.el
@@ -2,16 +2,20 @@
   (add-to-list 'company-backends 'company-jedi)
   (company-mode)
 
+  (use-package flycheck
+    :ensure t
+    :init (flycheck-mode))
+
   ;; (dumb-jump-mode)
   ;; (setq dumb-jump-force-searcher 'ag)
   ;; ;;disable navigation by tags
   ;; (setq xref-backend-functions (remq 'etags--xref-backend xref-backend-functions))
   ;; ;;Enable xref backend
   ;; (add-to-list 'xref-backend-functions #'dumb-jump-xref-activate t)
-  
+
   ;; (local-set-key (kbd "M-i") 'dumb-jump-go)
   ;; (local-set-key (kbd "C-M-i") 'dumb-jump-back)
-  
+
   )
 
 (add-hook 'python-mode-hook 'my-python-mode-hook)
