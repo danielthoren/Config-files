f554f0c199f5f5870f124d1b6356e9567e0564c1
danielthoren <danne_thoren456@hotmail.com>
2021-09-03 09:53:24 +0200

    Fish: Restored wsl launch script, still loops \n Emacs: Added git to 'use-package' declaration

--- fish/fish_files/functions/wsl_config.sh
+++ fish/fish_files/functions/wsl_config.sh
@@ -6,3 +6,10 @@ if grep -q WSL2 /proc/version; then
     export DISPLAY=$(route.exe print | grep 0.0.0.0 | head -1 | awk '{print $4}'):0.0
     export LIBGL_ALWAYS_INDIRECT=1
 fi
+
+fish
+
+if grep -q WSL2 /proc/version
+   set DISPLAY (route.exe print | grep 0.0.0.0 | head -1 | awk '{print $4}'):0.0
+   set LIBGL_ALWAYS_INDIRECT 1
+end
