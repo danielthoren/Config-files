8cd43c73fa993ac125bcba7d8f4f6c6ce3b176a8
danielthoren <danne_thoren456@hotmail.com>
2021-09-03 11:28:51 +0200

    Emacs: Added 'use-package' entry and wrote symlink script for windows

---
+++ emacs/conf.bat
@@ -0,0 +1,7 @@
+
+mklink %userprofile%\AppData\Roaming\.emacs.d\init.el %~dp0emacs_files\init.el
+
+mklink %userprofile%\AppData\Roaming\.emacs.d\settings %~dp0emacs_files\settings
+
+mklink %userprofile%\AppData\Roaming\.emacs.d\funs %~dp0emacs_files\funs
+
