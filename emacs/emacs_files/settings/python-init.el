5c95486a57f0485c2b37057fdf75f9aa1072f845
Daniel Thorén <daniel.thoren@syncore.se>
2021-09-03 11:54:25 +0200

    Emacs: Added jedi to use-package

--- emacs/emacs_files/settings/python-init.el
+++ emacs/emacs_files/settings/python-init.el
@@ -1,10 +1,6 @@
 (defun my-python-mode-hook ()
   (add-to-list 'company-backends 'company-jedi)
   (company-mode)
-
-  (use-package flycheck
-    :ensure t
-    :init (flycheck-mode))
   )
 
 (add-hook 'python-mode-hook 'auto-complete-mode)
