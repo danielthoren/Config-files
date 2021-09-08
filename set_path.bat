fbfaca4c9bdaeec21de7899ed3fb818ae746a787
Daniel Thorén <daniel.thoren@syncore.se>
2021-09-08 10:06:24 +0200

    Created .bat that adds importants paths to the PATH variable

---
+++ set_path.bat
@@ -0,0 +1,5 @@
+
+[Environment]::SetEnvironmentVariable("Path", $env:Path + ";C:\cygwin64\bin", "Machine")
+[Environment]::SetEnvironmentVariable("Path", $env:Path + ";C:\apps\GnuWin32\gnuwin32\bin", "Machine")
+
+[Environment]::SetEnvironmentVariable("Path", $env:Path + ";C:\Users\danth\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.9_qbz5n2kfra8p0\LocalCache\local-packages\Python39\Scripts", "Machine")
