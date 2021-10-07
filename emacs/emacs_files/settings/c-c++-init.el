44667b0c37405c93db3e3ee7bcebd44660ef1ab5
danielthoren <danne_thoren456@hotmail.com>
2021-10-07 08:44:38 +0200

    Emacs: Fixed indentation in c-c++-mode

--- emacs/emacs_files/settings/c-c++-init.el
+++ emacs/emacs_files/settings/c-c++-init.el
@@ -26,13 +26,14 @@ multiline comment prefix."
   (company-mode)
   (lsp-mode)
   (lsp)
+  (projectile-mode)
 
   (defun c-block-comment ()
     (interactive)
     (beginning-of-line)
     (open-line 1)
     (let* ((indent (current-column))
-	   (stars (make-string (- 78 indent) ?/)))
+	       (stars (make-string (- 78 indent) ?/)))
       (insert stars "\n")
       (indent-to indent)
       (insert "/**\n")
@@ -69,9 +70,22 @@ multiline comment prefix."
 
   (add-to-list 'auto-mode-alist '("\\.c\\'" . c++-mode))
 
-  ;; C
-  (setq c-basic-offset 4
-	c-default-style "linux")
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; C-mode stuff
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+  ;; my customizations for all of c-mode, c++-mode, objc-mode, java-mode
+  (c-set-offset 'substatement-open 0)
+  ;; other customizations can go here
+
+  (setq c++-tab-always-indent t)
+  (setq c-basic-offset 4)                  ;; Default is 2
+  (setq c-indent-level 4)                  ;; Default is 2
+
+  (setq tab-stop-list '(4 8 12 16 20 24 28 32 36 40 44 48 52 56 60))
+  (setq tab-width 4)
+  (setq indent-tabs-mode t)  ; use spaces only if nil
+
 
   (local-set-key (kbd "C-c m") 'cmake-ide-compile)
   (local-set-key (kbd "C-c i") 'indent-buffer)
