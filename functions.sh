1afcca055a8916eaf0538d4c7770888ac1d0ddc4
danielthoren <danne_thoren456@hotmail.com>
2021-11-15 13:33:29 +0100

    emacs: Updated installation of pyright from snapd to pip3

--- functions.sh
+++ functions.sh
@@ -9,7 +9,7 @@ command_exists () {
 }
 
 update() {
-    if ! $updated; then	    
+    if ! $updated; then
 	$APT_UPDATE
 	export updated=true
     fi
@@ -17,11 +17,11 @@ update() {
 
 install() {
     is_pkg_installed=$(dpkg-query -W --showformat='${Status}\n' $1 | grep "install ok installed")
-		       
+
     if ! [[ "${is_pkg_installed}" == "install ok installed" ]]; then
 	echo "$1 not installed, installing..."
 
-	update 
+	update
 	$APT_INSTALL "$1"
     fi
     echo "$output"
@@ -29,14 +29,20 @@ install() {
 
 add_source () {
     echo $1
-    
+
     if ! grep -q "^deb .*$1" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
 	echo "Adding repository ${the_ppa} to sources list..."
 	sudo add-apt-repository the_ppa
 	sudo apt update
-    fi    
+    fi
 }
+
 
 flag_exists () {
     [[ -n ${booleans[$1]} || -z ${booleans[$1]-foo} ]]
+}
+
+#Checks if in wsl (windows subsystem linux)
+in_wsl () {
+    grep -q Microsoft /proc/version
 }
