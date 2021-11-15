666c6f831e3efcc82d91f47127ad532cff720d09
danielthoren <danne_thoren456@hotmail.com>
2021-11-15 14:07:15 +0100

    fish: Added alias for wsl
    wslh : cd windows home folder
    wslg : cd windows git folder

--- fish/fish_files/functions/wsl_config.sh
+++ fish/fish_files/functions/wsl_config.sh
@@ -2,14 +2,15 @@
 
 #If in WSL, change display settings to use windows x-server
 
-if grep -q WSL2 /proc/version; then
-    export DISPLAY=$(route.exe print | grep 0.0.0.0 | head -1 | awk '{print $4}'):0.0
-    export LIBGL_ALWAYS_INDIRECT=1
+if ! grep -q WSL2 /proc/version; then
+    exit 0
 fi
+
+export DISPLAY=$(route.exe print | grep 0.0.0.0 | head -1 | awk '{print $4}'):0.0
+export LIBGL_ALWAYS_INDIRECT=1
 
 fish
 
-if grep -q WSL2 /proc/version
-   set DISPLAY (route.exe print | grep 0.0.0.0 | head -1 | awk '{print $4}'):0.0
-   set LIBGL_ALWAYS_INDIRECT 1
-end
+set DISPLAY route.exe print | grep 0.0.0.0 | head -1 | awk '{print $4}'
+set DISPLAY $DISPLAY:0.0
+set LIBGL_ALWAYS_INDIRECT 1
