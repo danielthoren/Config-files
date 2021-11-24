2db58aa9e8f957b271ea07a83099e2ca39e5bd39
danielthoren <danne_thoren456@hotmail.com>
2021-11-24 16:34:26 +0100

    emacs: Added multi cursor at mouse tip and updated key-sheat-cheat

--- emacs/emacs_files/init.el
+++ emacs/emacs_files/init.el
@@ -77,6 +77,7 @@
   (global-set-key (kbd "C->") 'mc/mark-next-like-this)
   (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
   (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+  (global-set-key (kbd "C-S-<mouse-1>") 'mc/add-cursor-on-click)
   :init
   (add-hook 'prog-mode-hook 'multiple-cursors-mode)
   )
