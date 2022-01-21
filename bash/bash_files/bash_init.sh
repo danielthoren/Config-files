3d54a0b07b9c8db5d6069d7258e1b2f69344a724
danielthoren <danne_thoren456@hotmail.com>
2022-01-21 10:56:04 +0100

    bash: Fixed bash config for WSL2 emacs: Removed files and moved csharp

---
+++ bash/bash_files/bash_init.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+######################################################################
+# Init WSL settings
+######################################################################
+
+#If in WSL, change display settings to use windows x-server
+#and add alias for windows home
+if grep -q WSL2 /proc/version; then
+    export DISPLAY=$(route.exe print | grep 0.0.0.0 | head -1 | awk '{print $4}'):0.0
+    export LIBGL_ALWAYS_INDIRECT=1
+
+    wslHome=$(wslpath $(wslvar USERPROFILE))
+
+    alias wslh="cd $wslHome"
+    alias wslg="cd $wslHome/git"
+fi
--- emacs/emacs_files/settings/markdown-init.el
+++
@@ -1,7 +0,0 @@
-(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
-(setq markdown-header-scaling t)
-
-(setq org-hide-emphasis-markers t)
-(setq org-fontify-done-headline t)
-
-(provide 'markdown-init)
