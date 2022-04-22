608cea7ef3ecf113c404f97c4bf95703d6443946
danielthoren <danne_thoren456@hotmail.com>
2022-04-22 14:33:32 +0200

    emacs: Dired now starts with dired-omit-mode enabled
    Dired defaults to hiding uninteresting files

--- emacs/emacs_files/init.el
+++ emacs/emacs_files/init.el
@@ -313,7 +313,9 @@
 ;; Configure dired+
 (setq diredp-hide-details-initially-flag nil) ;; If t, hide details by default
 (setq diredp-hide-details-propagate-flag t)   ;; If t, use previous hide/show scheme
-(set-face-foreground 'diredp-dir-name "green" )
+(set-face-foreground 'diredp-dir-name "green" ) ;; Set dirs color to green
+(add-hook 'dired-mode-hook (lambda () (dired-omit-mode))) ;; Hide uninteresting files by default
+
 ;; TODO: Bind function: diredp-move-named-in-kill-ring to key
 ;; TODO: Bind [C-0 w] to better key (copy files)
 
