8dfde24f87799e71265b3287de477de37fd40571
danielthoren <danne_thoren456@hotmail.com>
2022-12-31 11:38:24 +0100

    shell: Changed symlink path for common_bash
    Fixed incorrect symlink path

--- shell/fish/fish_files/config.fish
+++ shell/fish/fish_files/config.fish
@@ -2,7 +2,7 @@ set PATH ~/.local/bin $PATH
 
 export EDITOR='emacs'
 export FIFUNC='/home/'$USER'/git/Config-files'
-export COMMON_BASH_DIR="/$HOME/.config/common_bash"
+export COMMON_BASH_DIR="$HOME/.config/common_bash"
 
 # Source common alias betwee fish and bash
 source $COMMON_BASH_DIR/alias.sh
