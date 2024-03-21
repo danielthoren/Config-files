a0bebd051e7dbbe59689de17ea53ac0e0f38bab1
danielthoren <danne_thoren456@hotmail.com>
2024-03-21 11:27:46 +0100

    Minor improvements to conf.sh scripts
    * Redirected error output from 'rm' commands in 'emacs/conf.sh'.
    * Install 'software-properties-common' if command 'add-apt-repository'.
      is missing when executing function 'add_source'.
    * Add chmod to 'git/install_hooks' to make '.git/hooks/pre-commit' executable.
    * Updated 'todo.org'

--- git/install_hooks.sh
+++ git/install_hooks.sh
@@ -4,3 +4,4 @@ workingDir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 echo "Installing 'TODO' pre-commit hook"
 echo "$workingDir/hooks/todo_warning-pre-commit-hook.sh" >> .git/hooks/pre-commit
+chmod +rwx .git/hooks/pre-commit
